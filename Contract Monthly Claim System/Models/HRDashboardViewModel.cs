b2dbe81b4a69e4baee64184383821d3951b1992a
Ntando Nxumalo <ntando.codes@gmail.com>
2025-11-19 08:02:54 +0300

    Updates

---
+++ Contract Monthly Claim System/Models/HRDashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contract_Monthly_Claim_System.Models
+{
+    public class HRDashboardViewModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
